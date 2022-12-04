0354ed6407bae770f57723e180a7e4800328f924
Manas Chaturvedi <manaschaturvedi@LA-154.local>
2022-12-04 17:11:08 +0530

    reading env variables in Flask from dockercompose's environment

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 import psycopg2
+import os
 
 
 DB_HOST = 'db' # this will be the name of the service in the docker-compose file
-DB_NAME = 'my_database'
-DB_USER = 'postgres'
-DB_PASSWORD = 'my_password'
+DB_NAME = os.environ["POSTGRES_DB"]
+DB_USER = os.environ["POSTGRES_USER"]
+DB_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+
 
 def get_db_connection():
     conn = psycopg2.connect(
